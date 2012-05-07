1d42388648221d2f97de40ccec69b3fb859e9599
John Gietzen <otac0n@gietzen.us>
2012-05-07 12:39:40 -0700

    Stubbed out the `Card` model.

---
+++ CardCatalog/Models/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CardCatalog.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
