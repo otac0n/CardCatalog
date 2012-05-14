c738e745bde499c729e2629a70b842df1b413899
John Gietzen <otac0n@gietzen.us>
2012-05-13 18:03:26 -0700

    Added ownership count to the search index.

--- CardCatalog/Models/Indexes/CardSearch.cs
+++ CardCatalog/Models/Indexes/CardSearch.cs
@@ -7,40 +7,59 @@ using Raven.Abstractions.Indexing;
 
 namespace CardCatalog.Models.Indexes
 {
-    public class CardSearch : AbstractIndexCreationTask<Card, CardSearch.Result>
+    public class CardSearch : AbstractMultiMapIndexCreationTask<CardSearch.Result>
     {
         public CardSearch()
         {
-            this.Map = cards => from c in cards
-                                select new Result
-                                {
-                                    CardId = c.Id.ToString(),
-                                    Expansion = c.Expansion,
-                                    Rarity = c.Rarity,
-                                    Artist = string.Join(Environment.NewLine, c.NormalizedFaces.Select(f => f.Artist)),
-                                    Colors = string.Join(" ", c.Colors),
-                                    ConvertedManaCost = c.NormalizedFaces.Select(f => f.ConvertedManaCost).Where(x => x != null).FirstOrDefault(),
-                                    Name = string.Join(Environment.NewLine, c.NormalizedFaces.Select(f => f.Name).Where(x => !string.IsNullOrEmpty(x))),
-                                    Power = string.Join(Environment.NewLine, c.NormalizedFaces.Select(f => f.Power).Where(x => !string.IsNullOrEmpty(x))),
-                                    Text = string.Join(Environment.NewLine,
-                                        c.NormalizedFaces.Select(f => f.Name)
-                                        .Concat(c.NormalizedFaces.SelectMany(f => f.CardText))
-                                        .Concat(c.NormalizedFaces.Select(f => f.Types))
-                                        .Concat(c.NormalizedFaces.SelectMany(f => f.FlavorText))),
-                                    Toughness = string.Join(Environment.NewLine, c.NormalizedFaces.Select(f => f.Toughness).Where(x => !string.IsNullOrEmpty(x))),
-                                    Types = string.Join(Environment.NewLine, c.NormalizedFaces.Select(f => f.Types).Where(x => !string.IsNullOrEmpty(x))),
-                                };
+            this.AddMap<Card>(cards => from c in cards
+                                       select new Result
+                                       {
+                                           CardId = c.Id.ToString(),
+                                           Expansion = c.Expansion,
+                                           Owned = 0,
+                                           Rarity = c.Rarity,
+                                           Artist = string.Join(Environment.NewLine, c.NormalizedFaces.Select(f => f.Artist)),
+                                           Colors = string.Join(" ", c.Colors),
+                                           ConvertedManaCost = c.NormalizedFaces.Select(f => f.ConvertedManaCost).Where(x => x != null).FirstOrDefault(),
+                                           Name = string.Join(Environment.NewLine, c.NormalizedFaces.Select(f => f.Name).Where(x => !string.IsNullOrEmpty(x))),
+                                           Power = string.Join(Environment.NewLine, c.NormalizedFaces.Select(f => f.Power).Where(x => !string.IsNullOrEmpty(x))),
+                                           Text = string.Join(Environment.NewLine,
+                                               c.NormalizedFaces.Select(f => f.Name)
+                                               .Concat(c.NormalizedFaces.SelectMany(f => f.CardText))
+                                               .Concat(c.NormalizedFaces.Select(f => f.Types))
+                                               .Concat(c.NormalizedFaces.SelectMany(f => f.FlavorText))),
+                                           Toughness = string.Join(Environment.NewLine, c.NormalizedFaces.Select(f => f.Toughness).Where(x => !string.IsNullOrEmpty(x))),
+                                           Types = string.Join(Environment.NewLine, c.NormalizedFaces.Select(f => f.Types).Where(x => !string.IsNullOrEmpty(x))),
+                                       });
+
+            this.AddMap<Ownership>(ownerships => from o in ownerships
+                                                 select new Result
+                                                 {
+                                                     CardId = o.CardId,
+                                                     Owned = 1,
+                                                     Expansion = string.Empty,
+                                                     Rarity = string.Empty,
+                                                     Artist = string.Empty,
+                                                     Colors = string.Empty,
+                                                     ConvertedManaCost = -1,
+                                                     Name = string.Empty,
+                                                     Power = string.Empty,
+                                                     Text = string.Empty,
+                                                     Toughness = string.Empty,
+                                                     Types = string.Empty,
+                                                 });
 
             this.Reduce = results => from r in results
                                      group r by r.CardId into g
                                      select new Result
                                      {
                                          CardId = g.Key,
+                                         Owned = g.Sum(r => r.Owned),
                                          Expansion = g.Select(r => r.Expansion).Where(x => !string.IsNullOrEmpty(x)).FirstOrDefault(),
                                          Rarity = g.Select(r => r.Rarity).Where(x => !string.IsNullOrEmpty(x)).FirstOrDefault(),
                                          Artist = string.Join(Environment.NewLine, g.Select(r => r.Artist).Where(x => !string.IsNullOrEmpty(x))),
                                          Colors = string.Join(" ", g.Select(r => r.Colors).Where(x => !string.IsNullOrEmpty(x))),
-                                         ConvertedManaCost = g.Select(r => r.ConvertedManaCost).Where(x => x != null).FirstOrDefault(),
+                                         ConvertedManaCost = g.Select(r => r.ConvertedManaCost).Where(x => x != null && x != -1).FirstOrDefault(),
                                          Name = string.Join(Environment.NewLine, g.Select(r => r.Name).Where(x => !string.IsNullOrEmpty(x))),
                                          Power = string.Join(Environment.NewLine, g.Select(r => r.Power).Where(x => !string.IsNullOrEmpty(x))),
                                          Text = string.Join(Environment.NewLine, g.Select(r => r.Text).Where(x => !string.IsNullOrEmpty(x))),
@@ -67,6 +86,8 @@ namespace CardCatalog.Models.Indexes
             public string CardId { get; set; }
 
             public string Expansion { get; set; }
+
+            public int Owned { get; set; }
 
             public string Rarity { get; set; }
 
