c52f84357f4ccab24d44731456bb2ae41ae7f3dc
John Gietzen <otac0n@gietzen.us>
2012-06-03 15:32:58 -0700

    Added knockout-graph.js

---
+++ CardCatalog/Scripts/knockout-graph.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="~/Scripts/knockout-2.1.0.debug.js" />
+/// <reference path="~/Scripts/raphael.js" />
+/// <reference path="~/Scripts/g.raphael.js" />
+
+ko.bindingHandlers.graph = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        var value = ko.utils.unwrapObservable(valueAccessor());
+    },
+    update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        var value = ko.utils.unwrapObservable(valueAccessor());
+    }
+};
